c7bf673befa1b277854cf4418838715b003774ca
Kevin Dang <kevin@kevins-mac-mini.edwardstech.net>
2018-06-21 18:08:23 -0700

    Hover effects

---
+++ main.js
@@ -0,0 +1,52 @@
+function mouseOver1(){
+    document.getElementById("nyc").style.textDecoration = "underline";
+}
+
+function mouseOver2(){
+    document.getElementById("por").style.textDecoration = "underline";
+}
+
+
+function mouseOver3(){
+    document.getElementById("la").style.textDecoration = "underline";
+}
+
+
+function mouseOver4(){
+    document.getElementById("no").style.textDecoration = "underline";
+}
+
+
+
+function mouseOver5(){
+    document.getElementById("chi").style.textDecoration = "underline";
+}
+
+function mouseOver6(){
+    document.getElementById("sea").style.textDecoration = "underline";
+}
+
+function mouseOut1(){
+    document.getElementById("nyc").style.textDecoration = "none";
+}
+
+function mouseOut2(){
+    document.getElementById("por").style.textDecoration = "none";
+}
+
+function mouseOut3(){
+    document.getElementById("la").style.textDecoration = "none";
+}
+
+function mouseOut4(){
+    document.getElementById("no").style.textDecoration = "none";
+}
+
+function mouseOut5(){
+    document.getElementById("chi").style.textDecoration = "none";
+}
+
+function mouseOut6(){
+    document.getElementById("sea").style.textDecoration = "none";
+}
+
